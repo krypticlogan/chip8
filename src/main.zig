e3812a4ccb6bd23d9c4073c0cbee6357e85b1497
Logan <logan.zchjones@gmail.com>
2025-02-11 04:06:31 -0700

    IMPLEMENTING CODES

--- src/main.zig
+++ src/main.zig
@@ -51,6 +51,8 @@ fn init0u2Array(array: []u2) void {
         array[i] = 0;
     }
 }
+const gfxHeight = 32;
+const gfxWidth = 64;
 ///defines a chip-8 cpu made of all its components
 const CPU = struct {
     ///current instruction
@@ -63,14 +65,14 @@ const CPU = struct {
     ///program counter
     pc: u16 = undefined,
     ///graphics
-    gfx: [64 * 32]u2 = undefined,
+    gfx: [gfxHeight * gfxWidth]u2 = undefined,
 
     //timers
     delay_timer: timer = timer{},
     sound_timer: timer = timer{},
 
     ///program stack
-    stack: [16]u12 = undefined,
+    stack: [16]u16 = undefined,
     ///stack pointer
     sp: u5 = undefined,
     drawFlag: bool = false,
@@ -89,6 +91,7 @@ const CPU = struct {
         init0u8Array(&self.V);
         init0u2Array(&self.gfx);
         @memset(&self.stack, 0);
+        @memset(&self.gfx, 0);
 
         //reset values
         self.pc = 0x200;
@@ -115,65 +118,74 @@ const CPU = struct {
         self.pc += 2; // increment to next opcode
 
         //handle opcode (decode & execute)
-        switch(self.opcode & 0xF000){
-            0x0000 => {// multiple cases for 0x00__
-                switch(self.opcode & 0x000F) {
+        switch (self.opcode & 0xF000) {
+            0x0000 => { // multiple cases for 0x00__
+                switch (self.opcode & 0x000F) {
                     0x0000 => { // 00E0 (clears thw screen)
+                        @memset(&self.gfx, 0);
                     },
                     0x000E => { // 00EE (returns from a subroutine)
+                        self.sp -= 1;
+                        self.pc = self.stack[self.sp];
                     },
                     else => {
-                        print("Unknown opcode: 0x{x}\n", .{self.opcode});
+                        print("Opcode 0x0___: 0x{x} not handled", .{self.opcode});
                         return error.OpcodeNotDecoded;
                     },
                 }
             },
-            0x1000 => {// 1NNN (jump)
+            0x1000 => { // 1NNN (jump)
+                self.pc = self.opcode & 0x0FFF;
+            },
+            0x2000 => { // 2NNN (subroutine @ address NNN)
+                self.stack[self.sp] = self.pc;
+                self.sp += 1;
+                self.pc = self.opcode & 0x0FFF;
+            },
+            0x3000 => { // 3XNN (if Vx == NN) skip next instruction
                 return;
             },
-            0x2000 => {// 2NNN (subroutine @ address NNN)
+            0x4000 => { // 4XNN (if Vx != NN) skip next instruction
                 return;
             },
-            0x3000 => {// 3XNN (if Vx == NN) skip next instruction
+            0x5000 => { // (if Vx == Vy) skip next instruction
                 return;
             },
-            0x4000 => {// 4XNN (if Vx != NN) skip next instruction
-                return;
+            0x6000 => { // 6XNN (set register VX)
+                const X = (self.opcode & 0x0F00) >> 8;
+                self.V[X] = @as(u8, self.opcode & 0x00FF);
             },
-            0x5000 => {// (if Vx == Vy) skip next instruction
-                return;
-            },
-            0x6000 => {// 6XNN (set register VX)
-            },
-            0x7000 => {// 7XNN (add value to register VX)
+            0x7000 => { // 7XNN (add value to register VX)
+                const X = (self.opcode & 0x0F00) >> 8;
+                self.V[X] += self.opcode & 0x00FF;
             },
             0x8000 => {
-                switch(self.opcode & 0x000F) {
+                switch (self.opcode & 0x000F) {
                     0x0000 => { // 8XY0 (sets the value of Vx to the value of Vy)
                         return;
                     },
-                    0x0001 => {// 8XY1 (Sets VX to VX or VY. (bitwise OR operation))
+                    0x0001 => { // 8XY1 (Sets VX to VX or VY. (bitwise OR operation))
                         return;
                     },
-                     0x0002 => {// 8XY2 (Sets VX to VX and VY. (bitwise AND operation))
+                    0x0002 => { // 8XY2 (Sets VX to VX and VY. (bitwise AND operation))
                         return;
                     },
-                    0x0003 => {// 8XY3 (Sets VX to VX xor VY)
+                    0x0003 => { // 8XY3 (Sets VX to VX xor VY)
                         return;
                     },
-                    0x0004 => {// 8XY4 (Sets VX to VX xor VY)
+                    0x0004 => { // 8XY4 (Sets VX to VX xor VY)
                         return;
                     },
-                    0x0005 => {// 8XY5 (Sets VX to VX xor VY)
+                    0x0005 => { // 8XY5 (Sets VX to VX xor VY)
                         return;
                     },
-                    0x0006 => {// 8XY6 (Sets VX to VX xor VY)
+                    0x0006 => { // 8XY6 (Sets VX to VX xor VY)
                         return;
                     },
-                    0x0007 => {// 8XY7 (Sets VX to VX xor VY)
+                    0x0007 => { // 8XY7 (Sets VX to VX xor VY)
                         return;
                     },
-                    0x000E => {// 8XYE (Sets VX to VX xor VY)
+                    0x000E => { // 8XYE (Sets VX to VX xor VY)
                         return;
                     },
                     else => {
@@ -182,42 +194,63 @@ const CPU = struct {
                     },
                 }
             },
-            0x9000 => {//9XY0 if (Vx != Vy)	Skips the next instruction if VX does not equal VY. (Usually the next instruction is a jump to skip a code block)
-            
-            }, 
+            0x9000 => { //9XY0 if (Vx != Vy)	Skips the next instruction if VX does not equal VY. (Usually the next instruction is a jump to skip a code block)
+
+            },
             0xA000 => { // ANNN Sets the I address to NNN
                 self.I = self.opcode & 0x0FFF;
             },
             0xB000 => { //BNNN PC = V0 + NNN	Jumps to the address NNN plus V0
-               return;
+                return;
             },
             0xC000 => { //CNNN Vx = rand() & NN	Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN
-               return;
+                return;
             },
-            0xD000 => {// DXYN (display/draw)
+            0xD000 => { // DXYN (display/draw)
+                const X = (self.opcode & 0x0F00) >> 8;
+                const Y = (self.opcode & 0x00F0) >> 4;
+                const height = self.opcode & 0x000F;
+                const xPos = self.V[X] % gfxWidth;
+                const yPos = self.V[Y] & gfxHeight;
+                self.V[0xF] = 0;
+                for (0..height) |row| {
+                    const spriteByte = self.memory[self.I + row];
+                    for (0..8) |col| {
+                        const spritePixel = spriteByte & (0x80 >> col);
+                        const screenPixel = self.gfx[(yPos + row) * gfxWidth + (xPos + col)];
+                        if (spritePixel) {
+                            // Screen pixel also on - collision
+                            if (screenPixel == 1) {
+                                self.V[0xF] = 1;
+                            }
+
+                            // Effectively XOR with the sprite pixel
+                            screenPixel ^= 1;
+                        }
+                    }
+                }
             },
             0xE000 => {
-                switch(self.opcode & 0x000F){
-                    0x0001 => {// EXA1 if (key() != Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block)
+                switch (self.opcode & 0x000F) {
+                    0x0001 => { // EXA1 if (key() != Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block)
 
                     },
-                    0x000E => {// EX9E if (key() == Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is pressed (usually the next instruction is a jump to skip a code block)
+                    0x000E => { // EX9E if (key() == Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is pressed (usually the next instruction is a jump to skip a code block)
                     },
-                    0x0007 => {// EXA1 if (key() != Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block)
+                    0x0007 => { // EXA1 if (key() != Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block)
 
                     },
-                    0x000A => {// EXA1 if (key() != Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block)
+                    0x000A => { // EXA1 if (key() != Vx)	Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block)
 
                     },
                     else => {
                         print("Opcode 0xE___: 0x{x} not handled", .{self.opcode});
                         return error.OpcodeNotDecoded;
                     },
-                    
                 }
             },
             0xF000 => {
-                switch(self.opcode & 0x00FF){
+                switch (self.opcode & 0x00FF) {
                     0x0007 => { // FX07 sets VX to the value of the delay tiner
                         return;
                     },
@@ -250,19 +283,17 @@ const CPU = struct {
                         return error.OpcodeNotDecoded;
                     },
                 }
-                    },
+            },
             else => {
-                print("Uninitialized, or invalid opcode: 0x{x}\n", 
-                .{self.opcode});
+                print("Uninitialized, or invalid opcode: 0x{x}\n", .{self.opcode});
                 return error.OpcodeNotDecoded;
-            }, 
+            },
         }
         //update timers
-        if(self.delay_timer.time > 0)
+        if (self.delay_timer.time > 0)
             self.delay_timer.countdown();
-        if(self.sound_timer.time > 0)
-        {
-            if(self.sound_timer.time == 1)
+        if (self.sound_timer.time > 0) {
+            if (self.sound_timer.time == 1)
                 print("BEEP!\n", .{});
             self.sound_timer.countdown();
         }
@@ -285,8 +316,8 @@ fn eventLoop() !void {
 }
 const nsPs = 1_000_000_000;
 fn sleep(seconds: isize) void {
-    std.time.sleep(seconds*nsPs);
-} 
+    std.time.sleep(seconds * nsPs);
+}
 
 pub fn main() !void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO) == false) {
@@ -294,19 +325,19 @@ pub fn main() !void {
         return error.InitializationFailed;
     }
     defer c.SDL_Quit();
-
-    const win = c.SDL_CreateWindow("Chip8", 500, 500, 0);
+    const width = 64;
+    const height = 32;
+    const win = c.SDL_CreateWindow("Chip8", width * 10, height * 10, 0);
     defer c.SDL_DestroyWindow(win);
 
     const renderer = c.SDL_CreateRenderer(win, "renderer");
     defer c.SDL_DestroyRenderer(renderer);
-    
+
     var cpu = CPU{};
     cpu.init();
 
     try cpu.cycle();
-    // try eventLoop();
-   
+    try eventLoop();
 
     // print("memory: {any}", .{cpu.memory});
 }
